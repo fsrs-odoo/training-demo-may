5e0e7163217e08945d6dcf9aeb884a95a5a616ec
Fernando <98416635+fsrs-odoo@users.noreply.github.com>
2023-05-08 15:45:45 -0700

    Initialize 16-academy

---
+++ academy/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Odoo Academy',
+    'summary': """Module to handle Course and Sessions""",
+    'description': """Module to handle:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    'license': 'OPL-1',
+    'author': 'fsrs',
+    'website': 'www.odoo.com',
+    'category': 'Custom Modules/Tech Training',
+    # Step 1 - Update dependencies
+    'depends': ['mail'],
+    'data': [
+        'security/academy_groups.xml',
+        'security/ir.model.access.csv',
+        'security/academy_security.xml',
+        'data/session_data.xml',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+        'views/session_views.xml',
+    ],
+    'demo': [
+        'demo/course_demo.xml',
+    ],
+    'application': True, 
+}
